386149081548c564aa2c9dfc2ec0c3a8b0442608
Collins Rotich <14885955+collinskrotich@users.noreply.github.com>
2023-03-30 10:56:00 +0300

    Added analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,17 @@
 import '@/styles/globals.css';
 import Sidebar from '../../components/Sidebar';
+import
+ { Analytics } 
+from
+ 
+'@vercel/analytics/react'
+;
 
 export default function App({ Component, pageProps }) {
   return (
   <Sidebar>
   <Component {...pageProps} /> 
+  <Analytics />
   </Sidebar>
   );
 }
