db8dbab2fcbaec998ad40624394648d1d747540e
Collins Rotich <14885955+collinskrotich@users.noreply.github.com>
2023-03-31 11:28:20 +0300

    Added spacing to the pages

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -6,19 +6,19 @@ const Users = () => {
   return (
     <div className='bg-gray-100 min-h-screen'>
         <div className='flex justify-between p-4'>
-            <h2 className='px-8'>Users</h2>
+            <h2 className='px-20'>Regions</h2>
             <h2>Welcome back, Collins</h2>
         </div>
         <div className='w-full m-auto px-10 border rounded-lg bg-white overflow-y-auto'>
             <div className='my-3 py-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'>
-                <span className='pl-8'>Name</span>
+                <span className='px-20'>Name</span>
                 <span className='sm:text-left text-right'>Email</span>
                 <span className='hidden md:grid'>Last Reading</span>
                 <span className='hidden sm:grid'>Device</span>
             </div>
             <ul>
                 {data.map((order, id) => (
-                    <li key={id} className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer">
+                    <li key={id} className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 px-20 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer">
                         <div className='flex items-center'>
                             <div className='bg-green-600 p-3 rounded-lg'>
                                 <BsPersonFill className='text-white'/>
