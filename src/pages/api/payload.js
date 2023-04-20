bb861ddff69541184c8ec764a05066fe84da6fb3
collinskrotich <kolinsr@gmail.com>
2023-04-20 08:52:01 +0300

    Retesting apiversion latest

--- src/pages/api/payload.js
+++ src/pages/api/payload.js
@@ -2,6 +2,7 @@ const AWS = require('aws-sdk');
 require('dotenv').config();
 
 AWS.config.update({
+    apiVersion: 'latest',
     region: process.env.AWS_DEFAULT_REGION,
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
