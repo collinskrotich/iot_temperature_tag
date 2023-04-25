e5ac221992f33b267fed6ced833f1365807ea9ff
collinskrotich <kolinsr@gmail.com>
2023-04-25 06:02:19 +0300

    testing v2

--- src/pages/temptag.jsx
+++ src/pages/temptag.jsx
@@ -5,7 +5,7 @@ export default function App() {
     const [dataa, setDataa] = useState([])
 
     useEffect(() => {
-        axios.get('/api/payloadv3')
+        axios.get('/api/payload')
         .then((result) => {
             console.log("Getting data from server ::::",result.data)
             const payloadData = result.data
@@ -35,17 +35,28 @@ return (
         </thead>
         <tbody>
             {dataa.map((item) => (
-            <tr key={item.timeStamp}>
-                <td class="border border-slate-300 ...">{item.timeStamp.S}</td>
-                <td class="border border-slate-300 ...">{item.payload.M.latitude.N}</td>
-                <td class="border border-slate-300 ...">{item.payload.M.temperature.N}</td>
-                <td class="border border-slate-300 ...">{item.payload.M.relativeHumidity.N}</td>
-                <td class="border border-slate-300 ...">{item.payload.M.pressure.N}</td>
-                <td class="border border-slate-300 ...">{item.payload.M.soundLevels.N}</td>
-                <td class="border border-slate-300 ...">{item.payload.M.deviceId.S}</td>
-                <td class="border border-slate-300 ...">{item.payload.M.longitude.N}</td>
-                <td class="border border-slate-300 ...">{item.payload.M.airQualityIndex.N}</td>
-            </tr>
+            // <tr key={item.timeStamp}>
+            //     <td class="border border-slate-300 ...">{item.timeStamp.S}</td>
+            //     <td class="border border-slate-300 ...">{item.payload.M.latitude.N}</td>
+            //     <td class="border border-slate-300 ...">{item.payload.M.temperature.N}</td>
+            //     <td class="border border-slate-300 ...">{item.payload.M.relativeHumidity.N}</td>
+            //     <td class="border border-slate-300 ...">{item.payload.M.pressure.N}</td>
+            //     <td class="border border-slate-300 ...">{item.payload.M.soundLevels.N}</td>
+            //     <td class="border border-slate-300 ...">{item.payload.M.deviceId.S}</td>
+            //     <td class="border border-slate-300 ...">{item.payload.M.longitude.N}</td>
+            //     <td class="border border-slate-300 ...">{item.payload.M.airQualityIndex.N}</td>
+            // </tr>
+                        <tr key={item.timeStamp}>
+                        <td class="border border-slate-300 ...">{item.timeStamp}</td>
+                        <td class="border border-slate-300 ...">{item.payload.temperature}</td>
+                        <td class="border border-slate-300 ...">{item.payload.latitude}</td>
+                        <td class="border border-slate-300 ...">{item.payload.relativeHumidity}</td>
+                        <td class="border border-slate-300 ...">{item.payload.pressure}</td>
+                        <td class="border border-slate-300 ...">{item.payload.soundLevels}</td>
+                        <td class="border border-slate-300 ...">{item.payload.deviceId}</td>
+                        <td class="border border-slate-300 ...">{item.payload.longitude}</td>
+                        <td class="border border-slate-300 ...">{item.payload.airQualityIndex}</td>
+                        </tr>
             ))}
         </tbody>
         </table>
